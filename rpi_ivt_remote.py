cba17b410f157abde77ae3eb6d420fb5954f4e9a
Jonas Norling <jonas@elemental.se>
2022-12-30 15:38:43 +0100

    Update to Python3

--- rpi_ivt_remote.py
+++ rpi_ivt_remote.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 # Control IVT Nordic Inverter heat pump over the IR interface.
 # This script is meant to be run on a Rapsberry Pi with an IR transmitter diode.
@@ -17,11 +17,13 @@ import argparse
 import logging
 import struct
 
+
 def reverse(n, bits):
     o = 0
     for i in range(0, bits):
         o |= ((n >> i) & 1) << (bits - i - 1)
     return o
+
 
 def encode_temperature(in_t):
     if in_t == 10:
@@ -31,11 +33,13 @@ def encode_temperature(in_t):
     t = in_t - 17
     return reverse(t, 4)
 
+
 def calculate_parity(message):
     parity = 0
     for byte in message:
         parity ^= byte
     return (parity & 0x0f) ^ parity >> 4
+
 
 # Encode data bytes to a pulse train
 # The IR protocol is based on T = 450 us time quanta.
@@ -44,23 +48,25 @@ def calculate_parity(message):
 #  - a 1 bit is encoded as 3 low T followed by 1 high T,
 def encode(message):
     T = 450
-    pulses = [ 8*T, 4*T, 1*T ]
+    pulses = [8*T, 4*T, 1*T]
     for byte in message:
         for bit in range(0, 8):
             if byte & (0x80 >> bit):
-                pulses += [ 3*T, 1*T ]
+                pulses += [3*T, 1*T]
             else:
-                pulses += [ 1*T, 1*T ]
+                pulses += [1*T, 1*T]
     return pulses
+
 
 # Hand off the message to the LIRC kernel module
 def send_message(message):
     log = logging.getLogger("rpi_ivt_remote")
     log.debug("Sending %s" % ["%02x" % n for n in message])
     pulses = encode(message)
-    with open("/dev/lirc0", "w") as lirc:
+    with open("/dev/lirc0", "wb") as lirc:
         encoded = struct.pack("=%di" % len(pulses), *pulses)
         lirc.write(encoded)
+
 
 # Encode and send a command
 def send_command(on=False, temp=20, fan=2, ion=False):
@@ -92,7 +98,8 @@ def send_command(on=False, temp=20, fan=2, ion=False):
 
     send_message(message)    
 
-if __name__ == "__main__":
+
+def run():
     logging.basicConfig(level=logging.DEBUG)
 
     description = "Control IVT Nordic Inverter heat pump over the IR interface"
@@ -134,3 +141,7 @@ if __name__ == "__main__":
             parser.error("Missing fan speed")
 
     send_command(on=on, temp=temp, fan=fan_value, ion=args.ion)
+
+
+if __name__ == "__main__":
+    run()
